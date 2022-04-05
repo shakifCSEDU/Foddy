a374eaafc6cbd90a2d2818646dd102986437f103
sakif <sakifsahriar@gmail.com>
2022-04-05 23:01:18 +0600

    shimmer effect added in RecipesFragment

--- app/src/main/java/com/example/foody/RecipesFragment.java
+++ app/src/main/java/com/example/foody/RecipesFragment.java
@@ -8,12 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class RecipesFragment extends Fragment {
+import com.facebook.shimmer.ShimmerFrameLayout;
 
+public class RecipesFragment extends Fragment {
+    private ShimmerFrameLayout shimmerFrameLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_recipes, container, false);
+        View view = inflater.inflate(R.layout.fragment_recipes, container, false);
+        shimmerFrameLayout = (ShimmerFrameLayout)view.findViewById(R.id.shimmerId);
+        shimmerFrameLayout.startShimmer();
+
+        return view;
     }
 }
